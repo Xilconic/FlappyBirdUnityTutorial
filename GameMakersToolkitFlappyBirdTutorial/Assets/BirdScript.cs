4d87f857e879cf2b43f439a1baecb5f50d449c02
xilconic <xilconic@gmail.com>
2024-03-06 17:24:18 +0100

    Trigger game over on bird hitting anything

--- GameMakersToolkitFlappyBirdTutorial/Assets/BirdScript.cs
+++ GameMakersToolkitFlappyBirdTutorial/Assets/BirdScript.cs
@@ -10,18 +10,31 @@ public class BirdScript : MonoBehaviour
     [Tooltip("Enables tweaking the strength of the wing-flap of the Bird, when pressing Space.")]
     public float flapStrength = 10;
 
+    [Tooltip("The game logic manager.")]
+    public LogicManagerScript LogicManager;
+
+    private bool _isAlive = true;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Assert(BirdPhysics != null, "'BirdPhysics' Rigidbody2D not assigned!");
+        Debug.Assert(LogicManager != null, "'LogicManager' LogicManagerScript not assigned!");
+        Debug.Assert(flapStrength > 0, "'flapStrength' must be greater than 0!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)) 
+        if(Input.GetKeyDown(KeyCode.Space) && _isAlive) 
         {
             BirdPhysics.velocity = Vector3.up * flapStrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        _isAlive = false;
+        LogicManager.TriggerGameOver();
     }
 }
--- GameMakersToolkitFlappyBirdTutorial/Assets/LogicManagerScript.cs
+++ GameMakersToolkitFlappyBirdTutorial/Assets/LogicManagerScript.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LogicManagerScript : MonoBehaviour
 {
     [Tooltip("The element displaying the score.")]
     public Text ScoreUI;
+    [Tooltip("The game over screen.")]
+    public GameObject GameOverScreen;
 
     private int _score = 0;
 
@@ -14,6 +17,7 @@ public class LogicManagerScript : MonoBehaviour
     void Start()
     {
         Debug.Assert(ScoreUI != null, "'ScoreUI' is not set to a Text!");
+        Debug.Assert(GameOverScreen != null, "'GameOverScreen' is not set to a GameObject!");
     }
 
     [ContextMenu("Increase Score")]
@@ -21,5 +25,17 @@ public class LogicManagerScript : MonoBehaviour
     {
         _score++;
         ScoreUI.text = _score.ToString();
+    }
+
+    [ContextMenu("Restart Game")]
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    [ContextMenu("Trigger game over")]
+    public void TriggerGameOver()
+    {
+        GameOverScreen.SetActive(true);
     }
 }
