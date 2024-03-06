d6efc19c839dde0b0114034760d2cac365081938
xilconic <xilconic@gmail.com>
2024-03-06 21:26:35 +0100

    Implement control ceiling and kill floor
    This prevents the player from being able to fly under or over the obstacles.
    
    Fixes https://github.com/Xilconic/FlappyBirdUnityTutorial/issues/1

--- GameMakersToolkitFlappyBirdTutorial/Assets/BirdScript.cs
+++ GameMakersToolkitFlappyBirdTutorial/Assets/BirdScript.cs
@@ -13,6 +13,12 @@ public class BirdScript : MonoBehaviour
     [Tooltip("The game logic manager.")]
     public LogicManagerScript LogicManager;
 
+    [Tooltip("Determines how deep the Bird character can go before it automatically dies.")]
+    public float KillFloorDepth = -6.5f;
+
+    [Tooltip("Determines how high the Bird character can go before it cannot be controlled.")]
+    public float ControlCeiling = 6.5f;
+
     private bool _isAlive = true;
 
     // Start is called before the first frame update
@@ -26,13 +32,23 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && _isAlive) 
+        if(Input.GetKeyDown(KeyCode.Space) && _isAlive && transform.position.y <= ControlCeiling) 
         {
             BirdPhysics.velocity = Vector3.up * flapStrength;
+        }
+
+        if(transform.position.y < KillFloorDepth)
+        {
+            BecomeDead();
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        BecomeDead();
+    }
+
+    private void BecomeDead()
     {
         _isAlive = false;
         LogicManager.TriggerGameOver();
